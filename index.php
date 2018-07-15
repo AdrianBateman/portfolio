926d023bab9cd6abc7b7f19492fca82c37d0e2a2
Adrian Bateman <adrian@a-bateman.co.uk>
2018-07-15 22:38:21 +0100

    Added loadjJsonFile class, loaded data via json object into index.php

--- index.php
+++ index.php
@@ -2,38 +2,32 @@
   $active = "home";
 
   include('./components/blocks/global-header.php');
+
+  include('./components/functions/global-functions.php');
+
+  echo '<div class="language__wrapper">';
+  
+  $skillsJsonObj = new loadJsonFile();
+
+  $skillsJsonObj -> set_FileUrl("./components/data/json/skills.json");
+
+  $skillsJsonData =  $skillsJsonObj -> get_jsonArr();
+
+  foreach ($skillsJsonData['skills'] as $key ){
+    
+     echo '
+     <a href="'. $key['url'] .'" target="_blank" title="'. $key['title'] .'" class="language__item">
+
+      <p class="language__title">'. $key['title'] .'</p> 
+
+      <img src="'. $key['image'] .'" width="'. $key['width'] .'" height="40" alt="'. $key['alt'] .'" class=""> 
+
+     </a>';
+
+  }
 ?>
-  <div class="language__wrapper">
-    <a href="" target="_blank" class="language__item">
-      <img src="/components/images/logos/HTML5_logo.svg" width="40" height="40" alt="Proficient in HTML" class="">
-    </a>
-    <a href="" target="_blank" class="language__item">
-      <img src="/components/images/logos/ampproject-official.svg" width="40" height="40" alt="Proficient with AMP" class="">
-    </a>
-    <a href="" target="_blank" class="language__item">
-      <img src="/components/images/logos/LESS_Logo.svg" width="90" height="40" alt="Great with LESS" class="">
-    </a>
-    <a href="" target="_blank" class="language__item">
-      <img src="/components/images/logos/javascript.svg" width="40" height="40" alt="Javascript developer" class="">
-    </a>
-    <a href="" target="_blank" class="language__item">
-      <img src="/components/images/logos/CSS3_logo.svg" width="40" height="40" alt="CSS pro" class="">
-    </a>
-    <a href="" target="_blank" class="language__item">
-      <img src="/components/images/logos/Amplience-star.svg" width="40" height="40" alt="Amplience integrator" class="">
-    </a>
-    <a href="" target="_blank" class="language__item">
-      <img src="/components/images/logos/Bootstrap-logo.svg" width="40" height="40" alt="Bootstrap master" class="">
-    </a>
-    <a href="" target="_blank" class="language__item">
-      <img src="/components/images/logos/magento-seeklogo.com.svg" width="40" height="40" alt="Magento modifyer" class="">
-    </a>
-    <a href="" target="_blank" class="language__item">
-      <img src="/components/images/logos/PHP-logo.svg" width="40" height="40" alt="PHP writer" class="">
-    </a>
-    <a href="" target="_blank" class="language__item">
-      <img src="/components/images/logos/github.svg" width="71" height="40" alt="Git hub user" class="">
-    </a>
+
+
   </div>
 
   <div class="article__wrapper">
