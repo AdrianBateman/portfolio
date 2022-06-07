52ce751cce39d8f6e44a44049229a57c7c75cfca
AdrianBateman <adrian@a-bateman.co.uk>
2022-06-07 14:26:38 +0100

    Update nav, move language outside of hero & style

--- App/Templates/Globals/header-hero.php
+++ App/Templates/Globals/header-hero.php
@@ -3,23 +3,11 @@
 function globalHeaderHeroTemplate(): string
 {
     return <<<END
-    <div class="header-hero">
+    <header class="header-hero">
         <h1 class="header-hero__title">
             <small class="header-hero__title--small">Adrian Bateman</small>
             Senior Website Developer
-            <ul class="header-hero__list header-hero-list header-hero__title--small">
-                <li class="header-hero-list__item">HTML</li>
-                <li class="header-hero-list__item">CSS</li>
-                <li class="header-hero-list__item">JS</li>
-                <li class="header-hero-list__item">SASS</li>
-                <li class="header-hero-list__item">LESS</li>
-            </ul>
-            <ul class="header-hero__list header-hero-list header-hero__title--small">
-                <li class="header-hero-list__item">OOPHP</li>
-                <li class="header-hero-list__item">NODE</li>
-                <li class="header-hero-list__item">Java</li>
-            </ul>
         </h1>
-    </div>
+    </header>
     END;
 }
--- App/Templates/Home/Home.php
+++ App/Templates/Home/Home.php
@@ -2,5 +2,22 @@
 
 function homeTemplate(): string
 {
-    return "<h1>Hello World!</h1>";
+    return <<<END
+    <aside class="language">
+        <ul class="language-list">
+            <li class="language-list__item">HTML</li>
+            <li class="language-list__item">CSS</li>
+            <li class="language-list__item">JS</li>
+            <li class="language-list__item">SASS</li>
+            <li class="language-list__item">LESS</li>
+        </ul>
+        <ul class="language-list">
+            <li class="language-list__item">OOPHP</li>
+            <li class="language-list__item">NODE</li>
+            <li class="language-list__item">Java</li>
+        </ul>
+    </aside>
+
+    <h1>Hello World!</h1>
+    END;
 }
