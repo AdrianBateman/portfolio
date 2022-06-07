d0263cf48f0eaa53aee328a03b9bba5c2e4c8b13
AdrianBateman <adrian@a-bateman.co.uk>
2022-06-07 12:57:23 +0100

    Update hero & style

--- App/Templates/Globals/header-hero.php
+++ App/Templates/Globals/header-hero.php
@@ -2,5 +2,13 @@
 
 function globalHeaderHeroTemplate(): string
 {
-    return '<header>Header Hero</header>';
+    return <<<END
+    <div class="header-hero">
+        <h1 class="header-hero__title">
+            <small class="header-hero__title--small">Adrian Bateman</small>
+            Senior Website Developer
+            <small class="header-hero__title--small">HTML5 CSS JS PHP NODE SASS NPM</small>
+        </h1>
+    </div>
+    END;
 }
