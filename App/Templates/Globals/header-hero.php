7b23570eb329d405d3dc6987b051ae11e58aee02
AdrianBateman <adrian@a-bateman.co.uk>
2022-06-08 19:36:30 +0100

    Update homepage for languages array, style for mobile

--- App/Templates/Globals/header-hero.php
+++ App/Templates/Globals/header-hero.php
@@ -5,8 +5,13 @@ function globalHeaderHeroTemplate(): string
     return <<<END
     <header class="header-hero">
         <h1 class="header-hero__title">
-            <small class="header-hero__title--small">Adrian Bateman</small>
+            <small class="header-hero__title--small">
+                Adrian Bateman
+            </small>
             Senior Website Developer
+            <small class="header-hero__title--small">
+                Building responsive web applications since 2012
+            </small>
         </h1>
     </header>
     END;
--- App/Templates/Home/Home.php
+++ App/Templates/Home/Home.php
@@ -2,23 +2,61 @@
 
 function homeTemplate(): string
 {
-    return <<<END
-    <aside class="language">
-        <ul class="language-list">
-            <li class="language-list__item">HTML</li>
-            <li class="language-list__item">CSS</li>
-            <li class="language-list__item">JS</li>
-            <li class="language-list__item">SASS</li>
-            <li class="language-list__item">LESS</li>
-        </ul>
-        <ul class="language-list">
-            <li class="language-list__item">OOPHP</li>
-            <li class="language-list__item">NODE</li>
-            <li class="language-list__item">Java</li>
-        </ul>
-    </aside>
 
-    <h2>
-    </h2>
+    $languageList = [
+        ['HTML', 'CSS', 'JS', 'SASS', 'LESS'],
+        ['OOPHP', 'Node', 'Java', 'SQL']
+    ];
+
+    $template = '<aside class="language">';
+
+    foreach ($languageList as $listItem) {
+        $template .= '<ul class="language-list">';
+
+        foreach ($listItem as $item) {
+            $template .= "<li class=\"language-list__item\">$item</li>";
+        }
+        unset($item);
+
+        $template .= '</ul>';
+    }
+
+    unset($listItem);
+
+    $template .= "</aside>";
+
+    $template .= <<<END
+    <h2 class="page-title">Projects</h2>
+
+    <article class="project-article">
+        <h1 class="project-article__title">
+            <a
+                href="https://www.dreams.co.uk"
+                rel="noopener noreferrer nofollow"
+            >
+                Dreams Ltd
+            </a>
+        </h1>
+        <p>
+            Originally joining as a junior web developer in order to help out with day
+            to day tasks, I quickly became a valued member of the online trading team.
+            Assisting with not only the day to day tasks but also improving the site
+            load speed, using tried and tested techniques. This helps improve customer
+            experience and overall bounce rate & spend rate.
+        </p>
+        <p>
+            We switched platforms from Magento to Hybris and Amplience to proved a
+            "Headless Web" experience. This meant that we had only one source of
+            information going to each required destination.
+        </p>
+
+        <p>
+            Along with Hybris, we developed a mobile version of the site, working closely
+            with Google. This version uses the same Amplience feed as Hybris but is build entirely
+            in-house in PHP with Google's AMP project to help page load times.
+        </p>
+    </article>
     END;
+
+    return $template;
 }
