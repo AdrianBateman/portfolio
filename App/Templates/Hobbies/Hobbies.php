bd220b4c2d3f9038172ed8ecd4fc80fc0103a833
AdrianBateman <adrian@a-bateman.co.uk>
2022-06-18 21:55:53 +0100

    Add images to hobbies

--- App/Templates/Hobbies/Hobbies.php
+++ App/Templates/Hobbies/Hobbies.php
@@ -3,7 +3,81 @@
 function hobbiesTemplate(): string
 {
     return <<<END
+    <h2 class="page-title">Art</h2>
+    <p>
+        When I am not coding, I love to kick back do some creative works.
+        I use a range of media including digital
+    </p>
 
+    <h2>Inkscape</h2>
+
+    <img
+        src="/img/inkscape/natural-healing-shop-logo.jpg"
+        alt=""
+    >
+
+    <img
+        src="/img/inkscape/natural-healing-banner.png"
+        alt=""
+        style="background-color:white"
+    >
+
+    <img
+        src="/img/inkscape/ufo-pattern.svg"
+        alt=""
+        style="background-color:white"
+    >
+
+    <img
+        src="/img/inkscape/mugen-civic-type-r.jpg"
+        alt=""
+    >
+
+    <img
+        src="/img/inkscape/triumph.jpg"
+        alt=""
+    >
+
+    <h2>Gimp</h2>
+
+    <img
+        src="/img/gimp/angel.jpg"
+        alt=""
+    >
+
+    <img
+        src="/img/gimp/star.jpg"
+        alt=""
+    >
+
+    <h2>Pencil Drawings</h2>
+
+    <img
+        src="/img/pencil/houses-come-in-two.jpg"
+        alt="Pencil drawing of a house, half in ruin, half in good condition"
+    >
+
+    <h2>MyPaint</h2>
+
+    <img
+        src="/img/my-paint/house.jpg"
+        alt="First drawing in MyPaint of a house"
+    >
+
+    <img
+        src="/img/my-paint/tree.png"
+        alt="Drawing in pencil of a tree"
+    >
+
+    <img
+        src="/img/my-paint/white-eagle.jpg"
+        alt="Pencil style drawing"
+    >
+
+    <img
+        src="/img/my-paint/logo-idea-pencil.png"
+        alt="Idea for pencil style logo"
+    >
 
     END;
 }
