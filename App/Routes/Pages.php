d28ee91eda8e06c8f8ad65f21dbb576ee709383c
AdrianBateman <adrian@a-bateman.co.uk>
2022-06-25 17:31:08 +0100

    Add images, captions WIP

--- App/Routes/Pages.php
+++ App/Routes/Pages.php
@@ -19,13 +19,100 @@ Route\Route::add(
 Route\Route::add(
     '/hobbies',
     function () {
+        $images = [
+            [
+                "src" => "/img/inkscape/natural-healing-shop-logo.jpg",
+                "alt" => "Logo for website: Natural Healing",
+                "width" => 900,
+                "height" => 495,
+                "description" => "Logo designed for an online shop."
+            ],
+            [
+                "src" => "/img/inkscape/natural-healing-banner.png",
+                "alt" => "Banner design for website: Natural Healing",
+                "width" => 650,
+                "height" => 150,
+                "description" => "Banner incorperating logo design with website title."
+            ],
+            [
+                "src" => "/img/inkscape/ufo-pattern.svg",
+                "alt" => "UFO design with lines",
+                "width" => 501,
+                "height" => 331,
+                "description" => "Line art, originaly on A3 paper, transformed into SVG for web."
+            ],
+            [
+                "src" => "/img/inkscape/mugen-civic-type-r.jpg",
+                "alt" => "Civic car line art",
+                "width" => 900,
+                "height" => 637,
+                "description" => "Line art inspired by Civic Type R car."
+            ],
+            [
+                "src" => "/img/inkscape/triumph.jpg",
+                "alt" => "Triumph line art",
+                "width" => 900,
+                "height" => 637,
+                "description" => "Line art inspired by Triumph car."
+            ],
+            [
+                "src" => "/img/gimp/angel.jpg",
+                "alt" => "Angel image",
+                "width" => 900,
+                "height" => 686,
+                "description" => "Digital Angel design from stock photograph"
+            ],
+            [
+                "src" => "/img/gimp/star.jpg",
+                "alt" => "Start idea for logo",
+                "width" => 400,
+                "height" => 400,
+                "description" => "Logo idea combining both vector and bitmap"
+            ],
+            [
+                "src" => "/img/pencil/houses-come-in-two.jpg",
+                "alt" => "Pencil drawing of two sideds to a house",
+                "width" => 900,
+                "height" => 675,
+                "description" => "Pencil drawing, done seperatly on two peices of A4, combined."
+            ],
+            [
+                "src" => "/img/my-paint/house.jpg",
+                "alt" => "House drawing test using myPaint",
+                "width" => 900,
+                "height" => 742,
+                "description" => "Digital drawing, using similar techniques to chalk on digital canvas."
+            ],
+            [
+                "src" => "/img/my-paint/tree.png",
+                "alt" => "Tree idea for website",
+                "width" => 338,
+                "height" => 600,
+                "description" => "Concept image for sidebar navigation. Digital drawing."
+            ],
+            [
+                "src" => "/img/my-paint/white-eagle.jpg",
+                "alt" => "Portrait drawing",
+                "width" => 900,
+                "height" => 840,
+                "description" => "Digital portrait inspited by Native American culture."
+            ],
+            [
+                "src" => "/img/my-paint/logo-idea-pencil.png",
+                "alt" => "Logo idea in pencil",
+                "width" => 448,
+                "height" => 256,
+                "description" => "Logo idea for personal portfolio, digital drawing."
+            ]
+        ];
+
         require_once TEMPLATES_PATH . 'Hobbies/Hobbies.php';
 
         $title = 'Hobbies';
 
         echo globalHeaderTemplate($title);
 
-        echo hobbiesTemplate();
+        echo hobbiesTemplate($images);
 
         echo globalFooterTemplate();
     }
--- App/Templates/Hobbies/Hobbies.php
+++ App/Templates/Hobbies/Hobbies.php
@@ -1,107 +1,26 @@
 <?php
 
-function hobbiesTemplate(): string
+function hobbiesTemplate($images): string
 {
-    return <<<END
-    <h2 class="page-title">Art</h2>
-    <p>
-        When I am not coding, I love to kick back do some creative works.
-        I use a range of media including digital
-    </p>
+    $template = '<h2 class="page-title">Art</h2>';
+    $template .= '<p class="page-title__intro">When not coding, I love to kick back and do some creative work. I use a range of media, both real and digital.</p>';
+    $template .= '<div class="hobbies">';
 
-    <h2>Inkscape</h2>
+    foreach ($images as $image) {
+        $template .= '<div class="hobbies-img">';
 
-    <img
-        src="/img/inkscape/natural-healing-shop-logo.jpg"
-        alt=""
-        width="900"
-        height="495"
-    >
+        $template .= '<img src="' . $image['src'] . '"';
+        $template .= 'alt="' . $image['alt'] . '"';
+        $template .= 'width="' . $image['width'] . '"';
+        $template .= 'height="' . $image['height'] . '" class="hobbies__image">';
+        if (array_key_exists('description', $image)) {
+            $template .= '<p class="hobbies-img__description">' . $image['description'] . '</p>';
+        }
 
-    <img
-        src="/img/inkscape/natural-healing-banner.png"
-        alt=""
-        style="background-color:white"
-        width="650"
-        height="150"
-    >
+        $template .= '</div>';
+    }
 
-    <img
-        src="/img/inkscape/ufo-pattern.svg"
-        alt=""
-        style="background-color:white"
-        width="501"
-        height="331"
-    >
+    $template .= '</div>';
 
-    <img
-        src="/img/inkscape/mugen-civic-type-r.jpg"
-        alt=""
-        width="900"
-        height="637"
-    >
-
-    <img
-        src="/img/inkscape/triumph.jpg"
-        alt=""
-        width="900"
-        height="637"
-    >
-
-    <h2>Gimp</h2>
-
-    <img
-        src="/img/gimp/angel.jpg"
-        alt=""
-        width="900"
-        height="686"
-    >
-
-    <img
-        src="/img/gimp/star.jpg"
-        alt=""
-        width="400"
-        height="400"
-    >
-
-    <h2>Pencil Drawings</h2>
-
-    <img
-        src="/img/pencil/houses-come-in-two.jpg"
-        alt=""
-        width="900"
-        height="675"
-    >
-
-    <h2>MyPaint</h2>
-
-    <img
-        src="/img/my-paint/house.jpg"
-        alt=""
-        width="900"
-        height="742"
-    >
-
-    <img
-        src="/img/my-paint/tree.png"
-        alt=""
-        width="338"
-        height="600"
-    >
-
-    <img
-        src="/img/my-paint/white-eagle.jpg"
-        alt=""
-        width="900"
-        height="840"
-    >
-
-    <img
-        src="/img/my-paint/logo-idea-pencil.png"
-        alt=""
-        width="448"
-        height="256"
-    >
-
-    END;
+    return $template;
 }
