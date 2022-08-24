e4656138baa75963ded78019abb7425001a09868
AdrianBateman <adrian@a-bateman.co.uk>
2022-08-24 21:10:53 +0100

    remove wizzypop game

--- App/Templates/Apps/Wizzy-pop.php
+++ App/Templates/Apps/Wizzy-pop.php
@@ -4,7 +4,8 @@ function wizzyPopTemplate(): string
 {
     $template = '<h2>Wizzy Pop</h2>';
     $template .= '<div id="app" title="Wizzy Popper Game"></div>';
-    $template .= '<script async src="dist/wizzy-pop.js"></script>';
+    $template .= '<script async src="../dist/wizzyPop.js"></script>';
+    $template .= '<link rel="stylesheet" href="../dist/wizzyPop.css">';
 
     return $template;
 }
