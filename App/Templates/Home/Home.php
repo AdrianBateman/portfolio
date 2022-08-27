c8f73a7afdf02070a60a265eae077a81fe781da4
AdrianBateman <adrian@a-bateman.co.uk>
2022-08-27 13:58:56 +0100

    Work section: content, style

--- App/Templates/Home/Home.php
+++ App/Templates/Home/Home.php
@@ -2,12 +2,7 @@
 
 function homeTemplate(): string
 {
-    require_once TEMPLATES_PATH . 'Home/language-list.php';
-
-    $template = getLaguageList();
-
-    $template .= '<section class="work-section"><h2 class="page-title">';
-
+    $template = '<section class="work-section"><h2 class="page-title">';
     $template .= '<svg style="margin: 0 1rem 0 0;" width="32" height="32" fill="none" stroke-width="2">';
     $template .= '<rect stroke="#ebebeb" width="14" height="14" x="1" y="16" />';
     $template .= '<rect stroke="#ebebeb" width="22" height="22" x="8" y="1" />';
